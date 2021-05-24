f329929aaa11ea228c385af5d60b8992dd87c07c
Rafael Moral Pareja <rafael.moral@adevinta.com>
2021-05-24 09:37:32 +0200

    add external modules

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
   plugins: [reactRefresh()],
+  build: {
+    rollupOptions: {
+      external: ['react', 'react-dom', 'prop-types']
+    }
+  },
   resolve: {
     alias: {
       src: path.resolve(__dirname, './src')
